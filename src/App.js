034b62de1224cfe37e96551b6b5627a4e9a40343
Karin Nordkvist <kontakt@karinnordkvist.se>
2020-12-08 14:55:00 +0100

    Initialised the API - WORKING

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { TalksList } from './components/TalksList';
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <TalksList />
     </div>
-  )
-}
+  );
+};
